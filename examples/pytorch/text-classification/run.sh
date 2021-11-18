bf080575b5d69465620a78ad2575da61d9e19eb4
十四 <ljw040426@gmail.com>
2021-11-18 15:28:09 -0800

    Update run.sh

--- examples/pytorch/text-classification/run.sh
+++ examples/pytorch/text-classification/run.sh
@@ -1,13 +1,20 @@
 
 export TASK_NAME=mrpc
 
-python run_glue.py \
-  --model_name_or_path bert-base-cased \
+python run_glue_no_trainer.py \
+  --model_name_or_path textattack/bert-base-uncased-MRPC \
   --task_name $TASK_NAME \
-  --do_train \
-  --do_eval \
-  --max_seq_length 128 \
+  --max_length 128 \
   --per_device_train_batch_size 32 \
+  --per_device_eval_batch_size 32 \
   --learning_rate 2e-5 \
+  --eval_step 20 \
   --num_train_epochs 3 \
-  --output_dir /tmp/$TASK_NAME/
+  --output_dir /content/drive/MyDrive/moffett.ai/transformers-experiments/text-classification \
+  --pruning_sparsity 0.9375 \
+  --prune \
+  --pruning_epochs 3 \
+  --pruning_frequency 20 \
+  --deploy_device none \
+  --group_size 64 \
+  --kd
